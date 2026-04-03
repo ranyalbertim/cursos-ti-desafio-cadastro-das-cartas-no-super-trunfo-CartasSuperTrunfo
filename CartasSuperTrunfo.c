1b171b2fc11efefb0716d77fbdd2c6217d7c22e0
Raniele Albertim <rany_albertim@hotmail.com>
2026-04-03 00:45:08 +0000

    codigo pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,7 @@
 int main() { 
 
 //VARIAVEIS DA CARTA 1 
+//Adicionado as variaveis de densidade p. e PIB per capita
 
 char estado1; 
 char codigocarta1 [50]; 
@@ -14,8 +15,12 @@ int populacao1;
 float area1; 
 float PIB1; 
 int turismo1; 
+float densidade_populacional1;
+float PIB_percapita1;
+
 
 //VARIAVEIS DA CARTA 2 
+//Adicionado as variaveis de densidade p. e PIB per capita
 
 char estado2; 
 char codigocarta2 [50]; 
@@ -24,6 +29,8 @@ int populacao2;
 float area2; 
 float PIB2; 
 int turismo2; 
+float densidade_populacional2;
+float PIB_percapita2;
 
 //ENTRADA DE DADOS 
 
@@ -52,6 +59,10 @@ scanf("%f", &PIB1);
 printf("Digite o número de pontos turísticos\n"); 
 scanf("%d", &turismo1); 
 
+//calculando a densidade populacional e o PIB per capita da carta 1 
+densidade_populacional1 = (float)populacao1 / area1;
+PIB_percapita1 = (float)PIB1/ populacao1;
+
 
 //Carta 2 
 
@@ -78,6 +89,10 @@ scanf("%f", &PIB2);
 printf("Digite o número de pontos turísticos\n"); 
 scanf("%d", &turismo2); 
 
+//calculando a densidade populacional e o PIB per capita da carta 2
+densidade_populacional2 = (float)populacao2 / area2;
+PIB_percapita2 = (float)PIB2/ populacao2;
+
 // Exibição dos Dados das Cartas: 
 //Carta 1 
 
@@ -97,6 +112,11 @@ printf("PIB: %.2f milhões de reais\n", PIB1);
 
 printf("Número de pontos turísticos: %d\n", turismo1); 
 
+printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional1);
+
+printf("PIB per Capita: %.2f reais\n", PIB_percapita1);
+
+printf("--------------\n");
 
 
 //Carta 2 
@@ -117,6 +137,9 @@ printf("PIB: %.2f milhões de reais\n", PIB2);
 
 printf("Número de pontos turísticos: %d\n", turismo2); 
 
+printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional2);
+
+printf("PIB per Capita: %.2f reais\n", PIB_percapita2);
 
 
 printf("Fim do cadastro das cartas!\n"); 
